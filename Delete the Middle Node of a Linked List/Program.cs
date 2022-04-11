a01030bbd4abf82476da8567e6c94f2b0571f624
jd_786 <jaydeep_shil@certree.com>
2022-04-11 23:15:23 +0530

    Add project files.

---
+++ Delete the Middle Node of a Linked List/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Delete_the_Middle_Node_of_a_Linked_List
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+
+    public class ListNode
+    {
+      public int val;
+      public ListNode next;
+      public ListNode(int val = 0, ListNode next = null)
+      {
+        this.val = val;
+        this.next = next;
+      }
+    }
+
+    public ListNode DeleteMiddle(ListNode head)
+    {
+      if (head.next == null) return null;
+      if (head.next.next == null)
+      {
+        head.next = null;
+        return head;
+      }
+      ListNode slow = head;
+      ListNode fast = head;
+      ListNode prev = head;
+      while (fast != null && fast.next != null)
+      {
+        prev = slow;
+        slow = slow.next;
+        fast = fast.next.next;
+      }
+      prev.next = slow.next;
+      return head;
+    }
+  }
+}
